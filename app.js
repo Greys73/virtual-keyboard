b931a0b444ece558682d012e97aba86ee10ed453
Vladimir Myazin <v.myazin@gmail.com>
2023-04-27 23:31:40 +0400

    feat: tune TextArea Thu, Apr 27, 2023 11:31:39 PM

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 /* eslint-disable import/extensions */
 import { keyboardJSON } from './assets/modules/variables.js';
 import loadJSON from './assets/modules/loader.js';
-import updateKeys from './assets/modules/renderer.js';
+import { updateKeys, createHTML } from './assets/modules/renderer.js';
 import keyEvent from './assets/modules/key-events.js';
 
 window.keys = null;
 
+function tuneTextarea() {
+  const obj = document.getElementsByClassName('text-area');
+  obj[0].addEventListener('blur', () => {
+    obj[0].focus();
+  });
+  obj[0].addEventListener('keydown', (e) => {
+    e.preventDefault();
+  });
+  obj[0].addEventListener('select', () => {
+    obj[0].selectionEnd = obj[0].selectionStart;
+  });
+}
+
 function pageLoaded() {
+  createHTML();
+  tuneTextarea();
   loadJSON(keyboardJSON).then((response) => {
     window.keys = response;
     updateKeys();
-    console.log(window.keys);
   });
 }
 
