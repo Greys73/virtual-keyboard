bbda27fd0db13772e3bbb5fab2c3e0d120581cdb
Vladimir Myazin <v.myazin@gmail.com>
2023-04-27 23:33:07 +0400

    refactor: generate base HTML in JS Thu, Apr 27, 2023 11:33:07 PM

--- assets/modules/renderer.js
+++ assets/modules/renderer.js
@@ -1,21 +1,32 @@
 /* eslint-disable import/extensions */
-import { SETTINGS } from './variables.js';
 import createObject from './obj-creator.js';
+import { calcSymbol } from './typing.js';
 
-export default function updateKeys() {
+export function updateKeys() {
   const { keys } = window;
   keys.forEach((val, id) => {
-    let type = 'def';
     if (!('DOMElement' in keys[id])) {
       createObject(val);
     }
-    if (/[a-zA-Zа-яА-ЯёЁ]/i.test(keys[id].langs[SETTINGS.lang][type])) { // exception for letters
-      type = (SETTINGS.shiftPressed !== SETTINGS.capsLock) ? 'mod' : 'def';
-    } else if (SETTINGS.shiftPressed) {
-      type = 'mod';
-    } else {
-      type = 'def';
-    }
-    keys[id].DOMElement.innerText = keys[id].langs[SETTINGS.lang][type];
+    keys[id].DOMElement.innerText = calcSymbol(keys[id]);
   });
+}
+
+export function createHTML() {
+  document.body.innerHTML = `
+  <main class="wrapper">
+    <section>
+      <textarea name="textArea" class="text-area" cols="80" rows="10" autofocus unselectable="on"></textarea>
+    </section>
+    <section>
+      <div class="keyboard">
+        <section class="range" id="range1"></section>
+        <section class="range" id="range2"></section>
+        <section class="range" id="range3"></section>
+        <section class="range" id="range4"></section>
+        <section class="range" id="range5"></section>
+      </div>
+    </section>
+  </main>
+  `;
 }
